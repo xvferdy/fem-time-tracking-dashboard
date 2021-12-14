4fc55f61e6dcd7028d915dfda0f33a3f5e28e1f5
xvferdy <xv.ferdy@gmail.com>
2021-12-14 15:25:50 +0800

    Try to add Animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,33 @@
 import React, { useEffect, useState, useContext } from "react";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 
 import { PeriodContext } from "../context/Period.context";
 
 import ellipsis from "../assets/images/icon-ellipsis.svg";
+
+const cardVariants = {
+	hidden: {
+		opacity: 0,
+		y: 100,
+	},
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 1.2,
+			ease: "backOut",
+		},
+	},
+};
+
+const contentVariants = {
+	hidden: {
+		y: 200,
+	},
+	visible: {
+		y: [1, 0],
+	},
+};
 
 function Card({ title, timeframes }) {
 	const period = useContext(PeriodContext);
@@ -16,26 +41,41 @@ function Card({ title, timeframes }) {
 		console.log("Card component rendered");
 	}, [period, timeframes]);
 
+	const [isShow, setIsShow] = useState(true);
 	return (
-		<div className={`card card--${title.toLowerCase().split(" ").join("-")}`}>
-			<div className="card__content">
-				<div className="card__content-title">
-					<p>{title}</p>
-					<img src={ellipsis} alt="" />
-				</div>
-				<div className="card__content-detail">
-					<h3>{current}hrs</h3>
-					<p>
-						{period === "weekly"
-							? "Last Week "
-							: period === "daily"
-							? "Yesterday "
-							: "Last Month "}
-						- {previous}hrs
-					</p>
-				</div>
-			</div>
-		</div>
+		<motion.div
+			className={`card card--${title.toLowerCase().split(" ").join("-")}`}
+			variants={cardVariants}
+			initial="hidden"
+			animate="visible"
+			// onClick={() => setIsShow(!isShow)}
+		>
+			<AnimatePresence>
+				{isShow && (
+					<motion.div
+						className="card__content"
+						variants={contentVariants}
+						exit={{ y: 200, opacity: 0 }}
+					>
+						<div className="card__content-title">
+							<p>{title}</p>
+							<img src={ellipsis} alt="" />
+						</div>
+						<div className="card__content-detail">
+							<h3>{current}hrs</h3>
+							<p>
+								{period === "weekly"
+									? "Last Week "
+									: period === "daily"
+									? "Yesterday "
+									: "Last Month "}
+								- {previous}hrs
+							</p>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</motion.div>
 	);
 }
 
